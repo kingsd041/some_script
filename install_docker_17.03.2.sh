8e63842d499dc723f8f01791bd4fb4170a2cba40
root <root@ip-172-31-2-37.eu-west-1.compute.internal>
2018-04-26 13:33:06 +0000

    add install_docker

---
+++ install_docker_17.03.2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+apt-get update
+
+apt-get -y  install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+apt-get install -y docker-ce=17.03.2~ce-0~ubuntu-xenial
