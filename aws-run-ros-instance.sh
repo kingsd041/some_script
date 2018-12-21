9a09470758d4a2ee8145a23e1581102f60764064
hailong <nicholas_ksd@hotmail.com>
2018-12-21 12:10:06 +0800

    Create aws-run-ros-instance.sh

---
+++ aws-run-ros-instance.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -x
+instance_types="
+r4.large
+m4.large
+t3.nano
+c4.large
+p2.xlarge
+z1d.large
+"
+image_id="ami-0bcf9ffc23e25eacb"
+count=1
+disk_size=30
+
+for instance_type in $instance_types;
+do
+instance_name="hailong-"${instance_type}
+aws ec2 run-instances \
+    --image-id $image_id \
+    --count $count \
+    --instance-type $instance_type \
+    --key-name hailong \
+    --security-group-ids sg-1ace2460 \
+    --subnet-id subnet-c8071f81 \
+    --block-device-mappings "[{\"DeviceName\":\"/dev/sda1\",\"Ebs\":{\"VolumeSize\":${disk_size},\"DeleteOnTermination\":false}}]"  \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${instance_name}}]"
+done
